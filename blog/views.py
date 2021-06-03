eefaaf00cf277706fd4c0799f3f27de63f9d1ef3
rangkynsai <rangkyn@gmail.com>
2021-06-03 08:25:18 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views.generic import (ListView,DetailView,CreateView,UpdateView,DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from .models import Post
+from .models import Contact
+def home(request):
+    context = {
+        'postss':Post.objects.all().order_by('date_posted')
+    }
+    return render(request,'blog/index.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+def AddContact(request):
+    email = request.POST['email']
+    content = request.POST['content']
+    detail = Contact(email=email,content=content)
+    print('Submited')
+    detail.save()
+    return HttpResponseRedirect('/')
+
+def About(request):
+    return render(request,'blog/about.html')
